c07c8b12a163b9485e088c80cd31147e373885e2
Ramzy Badawy <ramzy.badawy@gmail.com>
2020-01-10 21:11:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,8 @@
-class Dog
-  def initialize(name, breed = "Mutt")
-    @name = name
-    @breed = breed
-  end
-end
-
-
+class Person
+  def initialize (name)
+    @name = name 
+  end 
+end 
 
 
 
